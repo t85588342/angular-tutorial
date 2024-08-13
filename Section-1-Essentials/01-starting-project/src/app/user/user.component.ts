affea48b8314f46f72a83e26f1b7e85285dbd618
test85588342 <test85588342@gmail.com>
2024-08-13 17:58:06 +0530

    32. Using Signal Inputs

--- Section-1-Essentials/01-starting-project/src/app/user/user.component.ts
+++ Section-1-Essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, Input, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,24 @@ import { Component, computed, Input, signal } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {
-  @Input({ required: true }) avatar!: string; // @Input() marks avatar as settable property from outside
-  @Input({ required: true }) name!: string;
+// export class UserComponent {
+//   @Input({ required: true }) avatar!: string; // @Input() marks avatar as settable property from outside
+//   @Input({ required: true }) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+//   get imagePath() {
+//     return 'assets/users/' + this.avatar;
+//   }
+//   onSelectUser() {}
+// }
+
+// Signals Approach
+export class UserComponent {
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  });
+
   onSelectUser() {}
 }
