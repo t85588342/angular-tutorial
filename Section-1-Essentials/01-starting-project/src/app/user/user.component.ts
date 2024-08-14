eb2dc838840ad113ce95250ba1346ecc09b7b63f
test85588342 <test85588342@gmail.com>
2024-08-14 12:50:36 +0530

    39. Accepting Objects As Inputs & Adding Appropriate Typings

--- Section-1-Essentials/01-starting-project/src/app/user/user.component.ts
+++ Section-1-Essentials/01-starting-project/src/app/user/user.component.ts
@@ -17,18 +17,24 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string; // @Input() marks avatar as settable property from outside
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string; // @Input() marks avatar as settable property from outside
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
 
   @Output() select = new EventEmitter<string>();
   // select = output<string>(); // internally uses EventEmitter
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
 
