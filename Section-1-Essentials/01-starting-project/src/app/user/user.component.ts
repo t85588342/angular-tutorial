249fde512692b5d6fdf6eebc83b0d28fa2220d2a
test85588342 <test85588342@gmail.com>
2024-09-09 17:12:29 +0530

    47. Storing Data Models in Separate Files

--- Section-1-Essentials/01-starting-project/src/app/user/user.component.ts
+++ Section-1-Essentials/01-starting-project/src/app/user/user.component.ts
@@ -9,17 +9,19 @@ import {
   signal,
 } from '@angular/core';
 
+import { type User } from './user.model';
+
 // type User = {
 //   id: string;
 //   avatar: string;
 //   name: string;
 // };
 
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+// interface User {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
 
 @Component({
   selector: 'app-user',
